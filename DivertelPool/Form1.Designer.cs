793abb6e59bcfbdd6d6e38704f3f13f4b52df1b1
Holydeew <nelsonmoisao007@gmail.com>
2021-12-20 00:47:11 +0000

    more work

--- DivertelPool/Form1.Designer.cs
+++ DivertelPool/Form1.Designer.cs
@@ -29,13 +29,79 @@ namespace DivertelPool
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.usernamebox = new System.Windows.Forms.TextBox();
+            this.passwordbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(274, 366);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(232, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(360, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(360, 246);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // usernamebox
+            // 
+            this.usernamebox.Location = new System.Drawing.Point(306, 186);
+            this.usernamebox.Name = "usernamebox";
+            this.usernamebox.Size = new System.Drawing.Size(161, 22);
+            this.usernamebox.TabIndex = 3;
+            // 
+            // passwordbox
+            // 
+            this.passwordbox.Location = new System.Drawing.Point(306, 277);
+            this.passwordbox.Name = "passwordbox";
+            this.passwordbox.Size = new System.Drawing.Size(161, 22);
+            this.passwordbox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.passwordbox);
+            this.Controls.Add(this.usernamebox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox usernamebox;
+        private System.Windows.Forms.TextBox passwordbox;
     }
 }
 
--- DivertelPool/Form1.cs
+++ DivertelPool/Form1.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace DivertelPool
 {
     public partial class Form1 : Form
     {
+        SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Holydeew\Documents\DatabasePool.mdf;Integrated Security=True;Connect Timeout=30");
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Con.Open();
+            SqlDataAdapter sda = new SqlDataAdapter("select COUNT(*) from Staff where UserName= '" + usernamebox.Text + "' and Password= '" + passwordbox.Text + "'    ", Con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                Form mf = new Form();
+                mf.Show();
+                this.Hide();
+
+            }
+            else
+            {
+                MessageBox.Show("UserName ou Password Errado!");
+
+            }
+
+            Con.Close();
         }
     }
 }
